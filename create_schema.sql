14ab639a79a14148c53772b82fc2a0884b8a13ae
vterehov207 <v.terehov207@gmail.com>
2019-09-21 13:02:53 -0400

    a solution _2

--- create_schema.sql
+++ create_schema.sql
@@ -13,5 +13,6 @@ adding things about stuff and breaking things
 
 -- just some text for training
 --- dependents  varchar(255) NOT NULL
+-- greeting  varchar(255) NOT NULL
 
 -- Adding a needless comment here to practice Git - Soris
